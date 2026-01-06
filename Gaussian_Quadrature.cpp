36d6972636798c67b1399d9c99a82b40fbcb45cb
Md Saiduzzaman <saiduzzaman.015.sojib@gmail.com>
2026-01-07 01:24:49 +0600

    Create Gaussian_Quadrature.cpp

---
+++ Gaussian_Quadrature.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+// Helper Functions
+double transform_x(double t, double a, double b);
+double scale_weight(double w, double a, double b);
+// Gaussian Algorithms
+double solve_gauss_2(function<double(double)> f, double a, double b);
+double solve_gauss_3(function<double(double)> f, double a, double b);
+double solve_gauss_4(function<double(double)> f, double a, double b);
+// Comparison Algorithms
+double solve_trapezoidal(function<double(double)> f, double a, double b, int n);
+double solve_simpson(function<double(double)> f, double a, double b, int n);
+// Math & Data
+double target_function(double x);
+double exact_solution(double a, double b);
+void log_results(const string& filename, const vector<string>& methods, const vector<double>& errors);
+
+
+// START OF IMPLEMENTATION
