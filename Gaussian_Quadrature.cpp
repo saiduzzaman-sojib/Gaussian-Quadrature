ae4d75736218dc467685ca5ad8f80e785b037b48
Md Saiduzzaman <saiduzzaman.015.sojib@gmail.com>
2026-01-08 01:59:38 +0600

    Update Gaussian_Quadrature.cpp

--- Gaussian_Quadrature.cpp
+++ Gaussian_Quadrature.cpp
@@ -8,26 +8,23 @@
 
 using namespace std;
 
-// Helper Functions
 double transform_x(double t, double a, double b);
 double scale_weight(double w, double a, double b);
-// Gaussian Algorithms
 double solve_gauss_2(function<double(double)> f, double a, double b);
 double solve_gauss_3(function<double(double)> f, double a, double b);
 double solve_gauss_4(function<double(double)> f, double a, double b);
-// Comparison Algorithms
 double solve_trapezoidal(function<double(double)> f, double a, double b, int n);
 double solve_simpson(function<double(double)> f, double a, double b, int n);
-// Math & Data
 double target_function(double x);
 double exact_solution(double a, double b);
 void log_results(const string& filename, const vector<string>& methods, const vector<double>& errors);
 
-// START OF IMPLEMENTATION
 
+// Start Of Implementation
 double transform_x(double t, double a, double b) {
     return ((b - a) * t + (b + a)) / 2.0;
 }
+
 double scale_weight(double sum, double a, double b) {
     return ((b - a) / 2.0) * sum;
 }
@@ -36,23 +33,20 @@ double solve_gauss_2(function<double(double)> f, double a, double b) {
     
     const double t = 0.577350269; 
     const double w = 1.0;
-
     double sum = w * f(transform_x(-t, a, b)) + 
                  w * f(transform_x(t, a, b));
-
+    
     return scale_weight(sum, a, b);
 }
-// 3-Point Gauss-Legendre ---
 
+
+// 3-Point Gauss-Legendre
 double solve_gauss_3(function<double(double)> f, double a, double b) {
     
     const double t1 = 0.0; 
     const double t2 = 0.774596669; 
-    
-    
     const double w1 = 0.888888889; 
     const double w2 = 0.555555556; 
-
     double sum = w1 * f(transform_x(t1, a, b)) +
                  w2 * f(transform_x(-t2, a, b)) +
                  w2 * f(transform_x(t2, a, b));
@@ -60,41 +54,37 @@ double solve_gauss_3(function<double(double)> f, double a, double b) {
     return scale_weight(sum, a, b);
 }
 
-// --- MEMBER 5: 4-Point Gauss-Legendre ---
 
+// 4-Point Gauss-Legendre 
 double solve_gauss_4(function<double(double)> f, double a, double b) {
     
     const double t1 = 0.339981044; 
     const double t2 = 0.861136312; 
-
     const double w1 = 0.652145155;
     const double w2 = 0.347854845;
-
     double sum = w1 * f(transform_x(-t1, a, b)) + w1 * f(transform_x(t1, a, b)) +
                  w2 * f(transform_x(-t2, a, b)) + w2 * f(transform_x(t2, a, b));
 
     return scale_weight(sum, a, b);
 }
-// ---Trapezoidal Rule ---
-// Used for comparison with Gaussian methods
+
+
+//Trapezoidal Rule 
 double solve_trapezoidal(function<double(double)> f, double a, double b, int n) {
     double h = (b - a) / n;
     double sum = f(a) + f(b);
-
     for (int i = 1; i < n; i++) {
         sum += 2.0 * f(a + i * h);
     }
     return (h / 2.0) * sum;
 }
-// --- Simpson's 1/3 Rule ---
-// Higher accuracy comparison
 
+
+//Simpson's 1/3 Rule
 double solve_simpson(function<double(double)> f, double a, double b, int n) {
     if (n % 2 != 0) n++; 
-    
     double h = (b - a) / n;
     double sum = f(a) + f(b);
-
     for (int i = 1; i < n; i++) {
         if (i % 2 == 0)
             sum += 2.0 * f(a + i * h);
@@ -104,7 +94,9 @@ double solve_simpson(function<double(double)> f, double a, double b, int n) {
 
     return (h / 3.0) * sum;
 }
-// TEST FUNCTIONS 
+
+
+// Test Function
 double target_function(double x) {
     return x * exp(x);
 }
@@ -113,12 +105,13 @@ double exact_solution(double a, double b) {
     double F_a = exp(a) * (a - 1.0);
     return F_b - F_a;
 }
-// --- Data Logging ---
+
+//Data Logging 
 void log_results(const string& filename, const vector<string>& methods, const vector<double>& errors) {
     ofstream file(filename);
     
     if (file.is_open()) {
-        file << "Method,Error\n"; // CSV Header
+        file << "Method,Error\n"; 
         for (size_t i = 0; i < methods.size(); ++i) {
             file << methods[i] << "," << errors[i] << "\n";
         }
@@ -128,7 +121,9 @@ void log_results(const string& filename, const vector<string>& methods, const ve
         cerr << "Error: Could not open file." << endl;
     }
 }
-// --- Main Driver Function ---
+
+
+//Main Driver Function
 int main() {
     cout << "=== Gaussian Quadrature Project (Group B3) ===" << endl;
 
